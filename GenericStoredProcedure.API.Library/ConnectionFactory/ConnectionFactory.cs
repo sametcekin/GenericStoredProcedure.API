ec865441351472288c7736dccda0260445d2a4f0
Samet Cekin <sametcekin@outlook.com.tr>
2020-09-05 22:52:18 +0300

    Connection Factory created

---
+++ GenericStoredProcedure.API.Library/ConnectionFactory/ConnectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using GenericStoredProcedure.API.Library.ConnectionFactory.Interfaces;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace GenericStoredProcedure.API.Library.ConnectionFactory
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// There is an Generic DB Connection connection. You can change DefaultConnection from appsettings.json
+        /// </summary>
+        /// <returns>MySQL connection</returns>
+        public IDbConnection DbConnection()
+        {
+            return new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+    }
+}
---
+++ GenericStoredProcedure.API.Library/ConnectionFactory/Interfaces/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace GenericStoredProcedure.API.Library.ConnectionFactory.Interfaces
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection DbConnection();
+    }
+}
