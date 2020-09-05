88801210025591ee327ce07b5ea10f23dc854eb6
Samet Cekin <sametcekin@outlook.com.tr>
2020-09-05 22:53:29 +0300

    Base controller created

---
+++ GenericStoredProcedure.API/Controllers/GenericStoredProcedureController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GenericStoredProcedure.API.Library.Data.Interfaces;
+using GenericStoredProcedure.API.Library.DataModel;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GenericStoredProcedure.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenericStoredProcedureController : ControllerBase
+    {
+        private readonly IBaseStoredProcedure _genericStoredProcedure;
+
+        public GenericStoredProcedureController(IBaseStoredProcedure genericStoredProcedure)
+        {
+            _genericStoredProcedure = genericStoredProcedure;
+        }
+
+        // POST api/<GenericStoredProcedure>
+        [HttpPost]
+        public async Task<object> GetStoredProcedureResult([FromBody] StoredProcedureModel storredProcedureModel)
+        {
+            var result = await _genericStoredProcedure.GetStoredProcedureResult(storredProcedureModel);
+            return result;
+        }
+
+    }
+}
