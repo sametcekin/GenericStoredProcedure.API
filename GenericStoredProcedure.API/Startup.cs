c10cd08dfb838281a34c9aac807187564a1e203d
Samet Cekin <sametcekin@outlook.com.tr>
2020-09-05 22:53:43 +0300

    DI added

---
+++ GenericStoredProcedure.API/Startup.cs
@@ -0,0 +1,62 @@
+using GenericStoredProcedure.API.Library.ConnectionFactory;
+using GenericStoredProcedure.API.Library.ConnectionFactory.Interfaces;
+using GenericStoredProcedure.API.Library.Data.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using GenericStoredProcedure.API.Library.Data;
+
+namespace GenericStoredProcedure.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+            #region Interface and Class Mapping for DI
+
+            services.AddTransient<IBaseStoredProcedure, BaseStoredProcedure>();
+
+            //For Connection DB (MySQL,SQLServer...)
+            services.AddTransient<IConnectionFactory, ConnectionFactory>();
+
+            #endregion        
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
